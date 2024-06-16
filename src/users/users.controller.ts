f248f13c32c9bfadad6cf0c61173689e18494513
Shoroh1 <diy.syl.official@gmail.com>
2024-06-17 00:30:06 +0300

    create users tables for db

---
+++ src/users/users.controller.ts
@@ -0,0 +1,16 @@
+import {Body, Controller, Header, HttpCode, HttpStatus, Post} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {CreateUserDto} from "./dto/create-user.dto";
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post('/signup')
+    @HttpCode(HttpStatus.CREATED)
+    @Header('Content-type', 'application/json')
+    createUser(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto)
+    }
+
+}
