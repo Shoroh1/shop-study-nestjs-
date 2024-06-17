12c4186a67aa80ee09f0ae4060373e3f7de9770a
Vladislav Atsuta <diy.syl.official@gmail.com>
2024-06-17 19:55:18 +0300

    Create Auth method

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,12 @@
-import {IsNotEmpty} from "class-validator";
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
-    @IsNotEmpty()
-    readonly username: string;
+  @IsNotEmpty()
+  readonly username: string;
 
-    @IsNotEmpty()
-    readonly password: string;
+  @IsNotEmpty()
+  readonly password: string;
 
-    @IsNotEmpty()
-    readonly email: string
+  @IsNotEmpty()
+  readonly email: string;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,47 +1,47 @@
-import {Injectable} from '@nestjs/common';
-import {InjectModel} from "@nestjs/sequelize";
-import * as bcrypt from "bcrypt"
-import {User} from "./dto/users.model";
-import {CreateUserDto} from "./dto/create-user.dto";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import * as bcrypt from 'bcrypt';
+import { User } from './dto/users.model';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectModel(User)
-        private userModel: typeof User,
-    ) {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+
+  findOne(filter: {
+    where: { id?: string; username?: string; email?: string };
+  }): Promise<User> {
+    return this.userModel.findOne({ ...filter });
+  }
+
+  async create(
+    createUserDto: CreateUserDto,
+  ): Promise<User | { warningMessage: string }> {
+    const user = new User();
+    const existingByUserName = await this.findOne({
+      where: { username: createUserDto.username },
+    });
+    const existingByEmail = await this.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingByUserName) {
+      return { warningMessage: 'Пользователь с таким именем уже существует' };
     }
 
-    findOne(filter: {
-        where: { id?: string; username?: string; email?: string }
-    }): Promise<User> {
-        return this.userModel.findOne({...filter})
+    if (existingByEmail) {
+      return { warningMessage: 'Пользователь с таким email уже существует' };
     }
 
-    async create(createUserDto: CreateUserDto,
-    ): Promise<User | { warningMessage: string }> {
-        const user = new User();
-        const existingByUserName = await this.findOne({
-            where: { username: createUserDto.username}
-        });
-        const existingByEmail = await this.findOne({
-            where: {email: createUserDto.email}
-        })
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
 
-        if (existingByUserName){
-            return  { warningMessage: 'Пользователь с таким именем уже существует'};
-        }
+    user.username = createUserDto.username;
+    user.password = hashedPassword;
+    user.email = createUserDto.email;
 
-        if (existingByEmail) {
-            return  { warningMessage: 'Пользователь с таким email уже существует'}
-        }
-
-        const hashedPassword = await bcrypt.hash(createUserDto.password, 10)
-
-        user.username = createUserDto.username;
-        user.password = hashedPassword;
-        user.email = createUserDto.email;
-
-        return user.save();
-    }
+    return user.save();
+  }
 }
